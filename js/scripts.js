9e56a6093a6e2234d16ca63b918349ac81afadc7
Seth Callister <seth.callister3@gmail.com>
2024-03-31 21:17:39 -0600

    Revert "initial commit"
    This reverts commit 38a889c075a0e9d64940e4bde22c8103d102cffa.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,3 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
-
-// menu toggle
-function toggleMenu() {
-    document.getElementById("primaryNav").classList.toggle("open");
-}
-
-const menu = document.getElementById("hamburger");
-
-menu.addEventListener("click", toggleMenu);

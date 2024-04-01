38a889c075a0e9d64940e4bde22c8103d102cffa
Seth Callister <seth.callister3@gmail.com>
2024-03-31 21:12:07 -0600

    initial commit

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
 // inject current year in footer
 const rightNow = new Date();
 document.querySelector('#year').textContent = rightNow.getFullYear();
+
+// menu toggle
+function toggleMenu() {
+    document.getElementById("primaryNav").classList.toggle("open");
+}
+
+const menu = document.getElementById("hamburger");
+
+menu.addEventListener("click", toggleMenu);
